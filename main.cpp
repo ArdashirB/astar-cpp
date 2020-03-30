af3a887ec927f0b52db80f3369830c965d2b1055
Ardashir Bulsara <abulsar@clemson.edu>
2020-03-30 18:24:26 -0400

    commit

--- main.cpp
+++ main.cpp
@@ -188,15 +188,10 @@ void PrintBoard(const vector<vector<State>> board) {
 
 int main() {
   int init[2]{0, 0};
-  int goal[2]{4, 5};
+  int goal[2]{9, 5};
   auto board = ReadBoardFile("1.board");
   auto solution = Search(board, init, goal);
   PrintBoard(solution);
   // Tests
-  TestHeuristic();
-  TestAddToOpen();
-  TestCompare();
-  TestSearch();
-  TestCheckValidCell();
-  TestExpandNeighbors();
+
 }
